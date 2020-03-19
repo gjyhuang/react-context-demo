514e3499fa80c2e7eae4019ce5d1d1875fe21aec
gjyhuang <grace.jy.huang@gmail.com>
2020-03-19 18:54:09 -0400

    init: basic changes, css

---
+++ src/components/BookList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const BookList = () => {
+  return (
+    <div className="book-list">
+      <ul>
+        <li>Name of the Wind</li>
+        <li>The Way of Kings</li>
+        <li>The Final Empire</li>
+      </ul>
+    </div>
+  )
+}
+
+export default BookList;
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1>React Context Reading List</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar;
